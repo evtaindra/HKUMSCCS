648665146f55d7a81080953d10faacc3d5cb7ed9
Bennett <wmsheng@hku.hk>
2018-10-19 19:02:27 +0800

    github_1.1

--- app/src/main/java/hk/hku/cs/buttonnavigationtest/Admission/Procedures.java
+++ app/src/main/java/hk/hku/cs/buttonnavigationtest/Admission/Procedures.java
@@ -1,5 +1,7 @@
 package hk.hku.cs.buttonnavigationtest.Admission;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import hk.hku.cs.buttonnavigationtest.R;
 
@@ -17,11 +20,29 @@ import hk.hku.cs.buttonnavigationtest.R;
  *  创建者：    BENNETT
  *  创建时间：  2018/10/17 13:19
  *  描述：      TODO
- */public class Procedures extends Fragment{
+ */public class Procedures extends Fragment implements View.OnClickListener{
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.admission_procedures,null);
+        Button button1 = view.findViewById(R.id.admission_apply);
+        button1.setOnClickListener(this);
         return view;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.admission_apply:
+                Intent intent = new Intent();
+                intent.setData(Uri.parse("https://aal.hku.hk/tpg/programme/master-science-computer-science"));
+                intent.setAction(Intent.ACTION_VIEW);
+                this.startActivity(intent);
+                break;
+                default:
+                    break;
+        }
     }
 }
